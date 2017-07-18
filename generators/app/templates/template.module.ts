78674a84e8ba70abf221bbeeb2c7c5677f9d88b7
Miroslav Jonaš <meeroslav@users.noreply.github.com>
2017-07-19 01:35:51 +0200

    Update template.module.ts

--- generators/app/templates/template.module.ts
+++ generators/app/templates/template.module.ts
@@ -1,11 +1,11 @@
-import {NgModule} from '@angular/core';
-import {TranslateModule} from '@ngx-translate/core';
-import {CommonModule} from '@angular/common';
-import {SharedModule} from '../_common/shared/shared.module';
-import {PageModule} from '../_common/page/page.module';
+import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../_common/shared/shared.module';
+import { PageModule } from '../_common/page/page.module';
 import { routing } from './<%- componentNamePluralCamelCase %>.routing';
-import {<%- componentNamePascalCase %>Service} from './common/<%- componentNameCamelCase %>.service';
-import {<%- componentNamePluralPascalCase %>DashboardComponent} from './dashboard/<%- componentNamePluralCamelCase %>-dashboard.component';
+import { <%- componentNamePascalCase %>Service } from './common/<%- componentNameCamelCase %>.service';
+import { <%- componentNamePluralPascalCase %>DashboardComponent } from './dashboard/<%- componentNamePluralCamelCase %>-dashboard.component';
 
 @NgModule({
   imports: [
