aa3d45a4deda3a0781425e04cbf4e201734faecf
Miroslav Jonaš <meeroslav@users.noreply.github.com>
2017-07-19 01:36:39 +0200

    Update template.routing.ts

--- generators/app/templates/template.routing.ts
+++ generators/app/templates/template.routing.ts
@@ -3,10 +3,10 @@ import { Error404Component } from '../_common/shared/error/404.component';
 
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AuthGuard} from '../_common/shared/permissions/auth-guard.service';
+import { AuthGuard } from '../_common/shared/permissions/auth-guard.service';
 
 const routes: Routes = [
-  { path: '',    component: <%- componentNamePluralPascalCase %>DashboardComponent, data: {'claims': 'Manage<%- componentNamePluralPascalCase %>'}, canActivate: [AuthGuard] },
+  { path: '', component: <%- componentNamePluralPascalCase %>DashboardComponent, data: {'claims': 'Manage<%- componentNamePluralPascalCase %>'}, canActivate: [AuthGuard] },
   { path: '**', component: Error404Component }
 ];
 
