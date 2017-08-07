0a2b6c4ead2a5a6547ea81ab11193eb353c5b219
Srdjan Pajic <spajic@greentube.com>
2017-08-07 15:56:50 +0200

    adding a search methods, translations, updated config file

--- generators/form/index.js
+++ generators/form/index.js
@@ -6,36 +6,15 @@ const _ = require('lodash');
 const fs = require('fs');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the fine ' + chalk.red('generator-hub-dashboard') + ' generator!'
-    ));
-
-    const prompts = [
-      {
-        type    : 'input',
-        name    : 'component_name',
-        message : 'Name for this Component in singular'
-      },
-      {
-        type    : 'input',
-        name    : 'component_name_plural',
-        message : 'Name for this Component in plural'
-      }
-    ];
+  setup() {
 
     let formConfig = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = formConfig;
-      this.props.componentNameCamelCase = _.camelCase(props.component_name);
-      this.props.componentNamePascalCase = _.capitalize(props.component_name);
-      this.props.componentNamePluralCamelCase = _.camelCase(props.component_name_plural);
-      this.props.componentNamePluralPascalCase = _.capitalize(props.component_name_plural);
-      this.props.componentNamePluralUpperCase = _.toUpper(props.component_name_plural);
-      this.props._ = _;
-    });
+    formConfig.componentNamePlural = formConfig.componentNamePlural || formConfig.componentName + 's';
+    formConfig.properties = _.flatten(formConfig.forms.map(f => f.properties));
+    console.log(formConfig.properties);
+    this.props = formConfig;
+    this.props._ = _;
+    return this.props;
   }
 
   writing() {
@@ -47,6 +26,11 @@ module.exports = class extends Generator {
     this.fs.copyTpl(
       this.templatePath('_template.form.ts'),
       this.destinationPath(`src/form.ts`),
+      this.props
+    );
+    this.fs.copyTpl(
+      this.templatePath('_en.json'),
+      this.destinationPath(`src/en.json`),
       this.props
     );
   }
--- generators/form/templates/_template.form.ts
+++ generators/form/templates/_template.form.ts
@@ -1,39 +1,56 @@
 import {Component, ElementRef, OnInit} from '@angular/core';
 import {WizardSlide} from '../../_common/custom-components/wizard/wizard-slide';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {LoadingIndicatorService} from '../../_common/shared/loading-indicator/loading-indicator.service';
+import 'rxjs/add/operator/toPromise';
 @Component({
-  selector: 'site-market-details',
-  templateUrl:  './site-market-details.component.html'
+  selector: '<%- _.kebabCase(componentName) %>',
+  templateUrl:  './<%- _.kebabCase(componentName) %>.component.html'
 })
-export class <%- componentNamePascalCase %>Component extends WizardSlide implements OnInit {
+export class <%- componentName %>Component extends WizardSlide implements OnInit {
   siteForm: FormGroup;
+  <% for(var i=0; i<properties.length; i++) {
+    if (properties[i].type !== 'autocomplete') continue;
+    -%>
+    <%- properties[i].collectionName %>: SelectItem[];
+    private all<%- _.capitalize(properties[i].collectionName) %>: Promise<<%- properties[i].collectionType %>[]>;
+  <% } -%>
 
   constructor(elementRef: ElementRef,
               loadingIndicatorService: LoadingIndicatorService,
               private formBuilder: FormBuilder) {
     super(elementRef, loadingIndicatorService);
-
   }
+
+  <% for(var i=0; i<properties.length; i++) {
+    var property = properties[i];
+    if (property.type !== 'autocomplete') continue;
+    -%>
+  search<%- _.capitalize(property.collectionName) %>(event: any) {
+    if (!this.all<%- _.capitalize(property.collectionName) %>) {
+      this.all<%- _.capitalize(property.collectionName) %> = this.service.get<%- _.capitalize(property.collectionName) %>().toPromise();
+    }
+    this.all<%- _.capitalize(property.collectionName) %>.then(<%- property.collectionName %> => {
+      this.<%- property.collectionName %> = <%- property.collectionName %>.filter(c => c.name === event.query);
+    });
+  }
+  <% } %>
+
 
   ngOnInit(): void {
     // load data
     this.prepareForm();
   }
-
-
   /**
    * Set form and validations
    */
   private prepareForm() {
     this.siteForm = this.formBuilder.group({
       // test
-    <% for(var j=0; j<forms.length; j++) { -%>
-    <% for(var i=0; i<forms[j].properties.length; i++) {
-      var property = forms[j].properties[i];
+    <% for(var i=0; i<properties.length; i++) {
+      var property = properties[i];
       -%>
       <%- property.id %>: ['', [<% if (property.required) { %>Validators.required<% } %>]],
-    <% } %>
     <% } %>
     });
 
